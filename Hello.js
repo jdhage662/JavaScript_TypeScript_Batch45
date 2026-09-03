c198a0ae477a29a399bc89e737ffec6b705d391c
jdhage662 <jdhage662@gmail.com>
2026-09-03 17:43:31 +0100

    Refactor Hello.js by removing classes and methods
    Removed class definitions and sound methods from Hello.js.

--- Hello.js
+++ Hello.js
@@ -3,20 +3,3 @@ b = 'Javascript' //string
 c = true   //boolen
 console.log("Hello "+a)
 age = 20;
-
-//command to print the out put on cmd console 
-
-class abc{
-
-makeSound(){
-    console.log('Moo')
-}
-
-}
-
-
-class xyz extends abc{
-    makeSound(){
-          console.log('Bark')
-    }
-}
