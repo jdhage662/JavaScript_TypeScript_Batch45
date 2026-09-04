fa430b62b3a5d59fb6e98757bc27cd71d42b370d
GiridharBiradar444 <giridharbiradar444@gmail.com>
2026-09-04 22:06:03 +0530

    goodday

---
+++ getdayoftheweak.ts
@@ -0,0 +1,22 @@
+function dayOfWeek(day:number){
+switch(day){
+    case 1:
+        console.log('Monday')
+        break;
+    case 2:
+        console.log('Tuesday')
+        break;
+    case 3:
+        console.log('Wensday')
+        break;
+    case 4:
+        console.log('Thursday')
+        break;
+    case 5:
+        console.log('Friday')
+        break; 
+    default:
+        console.log("Invalid output")           
+}
+}
+dayOfWeek(8)
